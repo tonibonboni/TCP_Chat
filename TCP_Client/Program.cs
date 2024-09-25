d5bff6f3a50fba9d6983b6a4da71a2b648145ef7
Kaloyan Tsankov <kaloqn.cankov@gmail.com>
2024-09-25 21:38:57 +0300

    Add project files.

---
+++ TCP_Client/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Client
+{
+    private const int Port = 8888;
+    private const string ServerIp = "127.0.0.1";
+
+    static void Main()
+    {
+        TcpClient client = new TcpClient(ServerIp, Port);
+        Console.WriteLine("Connected to server. Start chatting!");
+
+        NetworkStream stream = client.GetStream();
+
+        Thread receiveThread = new Thread(ReceiveMessages);
+        receiveThread.Start(stream);
+
+        while (true)
+        {
+            string message = Console.ReadLine();
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+
+    static void ReceiveMessages(object obj)
+    {
+        NetworkStream stream = (NetworkStream)obj;
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while (true)
+        {
+            try
+            {
+                bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine(message);
+            }
+            catch (Exception)
+            {
+                break;
+            }
+        }
+    }
+}
---
+++ TCP_Server/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Server
+{
+    private static readonly List<TcpClient> clients = new List<TcpClient>();
+    private const int Port = 8888;
+
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, Port);
+        server.Start();
+        Console.WriteLine($"Server started on port {Port}");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            clients.Add(client);
+            Thread clientThread = new Thread(HandleClient);
+            clientThread.Start(client);
+        }
+    }
+
+    static void HandleClient(object obj)
+    {
+        TcpClient tcpClient = (TcpClient)obj;
+        NetworkStream stream = tcpClient.GetStream();
+
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while (true)
+        {
+            try
+            {
+                bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Received: {message}");
+
+                BroadcastMessage(tcpClient, message);
+            }
+            catch (Exception)
+            {
+                break;
+            }
+        }
+
+        clients.Remove(tcpClient);
+        tcpClient.Close();
+    }
+
+    static void BroadcastMessage(TcpClient sender, string message)
+    {
+        byte[] broadcastBuffer = Encoding.ASCII.GetBytes(message);
+
+        foreach (TcpClient client in clients)
+        {
+            if (client != sender)
+            {
+                NetworkStream stream = client.GetStream();
+                stream.Write(broadcastBuffer, 0, broadcastBuffer.Length);
+            }
+        }
+    }
+}
